9c541092d08d6b1eda2072d4b937463c5109a78c
Daniel O'Connor <daniel.oconnor@gmail.com>
2016-03-21 15:53:52 +1030

    Add Yass ATDIS (very slow) scraper

---
+++ Gemfile
@@ -0,0 +1,3 @@
+source 'https://rubygems.org'
+
+gem 'atdisplanningalertsfeed', github: 'planningalerts-scrapers/atdisplanningalertsfeed'
---
+++ scraper.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+Bundler.require
+
+url = "http://mycouncil.yass.nsw.gov.au/Horizon/@@horizondap@@/atdis/1.0/"
+
+ATDISPlanningAlertsFeed.save(url)
