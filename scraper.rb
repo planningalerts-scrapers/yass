343a978ad5c860184886aa5bd423b5709585d542
Matthew Landauer <mlandauer@gmail.com>
2019-06-12 16:44:22 +1000

    Fail with more helpful error message

--- scraper.rb
+++ scraper.rb
@@ -1,7 +1,9 @@
 #!/usr/bin/env ruby
 Bundler.require
 
-ATDISPlanningAlertsFeed.save(
-  "http://mycouncil.yass.nsw.gov.au/Horizon/@@horizondap@@/atdis/1.0/",
-  "Sydney"
-)
+raise "ATDIS feed is no longer available. Now has a custom system so will need scraper rewrite. See https://github.com/planningalerts-scrapers/yass/issues/2"
+
+# ATDISPlanningAlertsFeed.save(
+#   "http://mycouncil.yass.nsw.gov.au/Horizon/@@horizondap@@/atdis/1.0/",
+#   "Sydney"
+# )
