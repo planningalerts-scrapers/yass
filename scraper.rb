f168d79775be561c232353baa0f298fa980a297a
Matthew Landauer <mlandauer@gmail.com>
2021-05-27 09:53:50 +1000

    Don't want the scraper erroring
    Because this causes lots of noise in the morph email alerts.
    
    https://github.com/planningalerts-scrapers/issues/issues/91

--- scraper.rb
+++ scraper.rb
@@ -1,7 +1,8 @@
 #!/usr/bin/env ruby
 Bundler.require
 
-raise "ATDIS feed is no longer available. Now has a custom system so will need scraper rewrite. See https://github.com/planningalerts-scrapers/yass/issues/2"
+puts "Scraper is broken. So, commenting it out to remove noise"
+puts "Now has a custom system so will need scraper rewrite. See https://github.com/planningalerts-scrapers/issues/issues/91"
 
 # ATDISPlanningAlertsFeed.save(
 #   "http://mycouncil.yass.nsw.gov.au/Horizon/@@horizondap@@/atdis/1.0/",
